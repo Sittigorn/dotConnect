bd5d32e1a3d21d743a29850b0eb3ff579ef53e8b
wispoz <wispoz@inbox.ru>
2014-11-07 11:48:19 +0400

    Добавление интерполяции.

--- core/src/com/wispoz/dotconnect/ActorInputListener.java
+++ core/src/com/wispoz/dotconnect/ActorInputListener.java
@@ -2,17 +2,25 @@ package com.wispoz.dotconnect;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.InputProcessor;
+import com.badlogic.gdx.math.Interpolation;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.actions.MoveToAction;
+import com.badlogic.gdx.utils.Pool;
+
 
 /**
  * Created by wispoz on 06.11.14.
  */
 public class ActorInputListener extends InputListener implements InputProcessor {
     Actor jet;
-
+    MoveToAction prev;
+    Pool<MoveToAction> pool = new Pool<MoveToAction>() {
+        protected MoveToAction newObject () {
+            return new MoveToAction();
+        }
+    };
     public ActorInputListener(Actor jet){
         this.jet = jet;
     }
@@ -69,13 +77,21 @@ public class ActorInputListener extends InputListener implements InputProcessor 
     @Override
     public boolean touchDown (InputEvent event, float x, float y, int pointer, int button) {
         Gdx.app.log("BUBBLE", "touchdown");
+
         return true;  // must return true for touchUp event to occur
     }
     @Override
     public void touchUp (InputEvent event, float x, float y, int pointer, int button) {
-        MoveToAction moveAction = new MoveToAction();
-        moveAction.setPosition(x, y);
-        moveAction.setDuration(10f);
-        this.jet.addAction(moveAction);
+        this.jet.removeAction(prev);
+         MoveToAction moveAction = pool.obtain();
+         moveAction.setPool(pool);
+         moveAction.setPosition(x, y);
+         moveAction.setDuration(10f);
+         moveAction.setInterpolation(Interpolation.bounceOut);
+         this.jet.addAction(moveAction);
+         this.prev = moveAction;
+
     }
+
+
 }
--- core/src/com/wispoz/dotconnect/Jet.java
+++ core/src/com/wispoz/dotconnect/Jet.java
@@ -13,7 +13,7 @@ import com.badlogic.gdx.scenes.scene2d.actions.MoveToAction;
  */
 public class Jet extends Actor {
 
-    Texture texture = new Texture(Gdx.files.internal("jet.png"));
+    Texture texture = new Texture(Gdx.files.internal("circle.png"));
     InputListener inputListener = new ActorInputListener(this);
        public Jet(){
            setBounds(getX(),getY(),texture.getWidth(),texture.getHeight());
@@ -21,7 +21,7 @@ public class Jet extends Actor {
        }
     @Override
     public void draw(Batch batch, float alpha){
-        batch.draw(texture,this.getX(),getY());
+        batch.draw(texture, getX(), getY());
     }
 
 }
