6b3acbdd5cd5e86f2f058c0a4e21ed947f7af8b3
wispoz <wispoz@inbox.ru>
2014-11-12 17:02:39 +0400

    viewport test

--- core/src/com/wispoz/dotconnect/BackgroundActor.java
+++ core/src/com/wispoz/dotconnect/BackgroundActor.java
@@ -1,6 +1,5 @@
 package com.wispoz.dotconnect;
 
-import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.Sprite;
@@ -16,7 +15,7 @@ public class BackgroundActor extends Actor {
     public BackgroundActor() {
         backgroundTexture = new Texture("Levels01night.png");
         backgroundSprite = new Sprite(backgroundTexture);
-       // backgroundSprite.setSize(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+      //  backgroundSprite.setSize(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
     }
 
     @Override
